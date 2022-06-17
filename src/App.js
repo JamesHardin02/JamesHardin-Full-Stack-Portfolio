199d932c10db84bea20641f5b9ccb8c60655ca8b
JamesH <jwhusc7@gmail.com>
2022-06-17 15:34:47 -0400

    added nav bar with titles that hightlight on click

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
   const [categories] = useState([
     {
       name: 'About Me',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+      description: 'Description of full stack web developer James Hardin',
     },
-    { name: 'Portfolio', description: 'Portraits of people in my life' },
-    { name: 'Contact', description: 'Delicious delicacies' },
-    { name: 'Resume', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: 'Portfolio', description: 'Projects completed by James Hardin' },
+    { name: 'Contact', description: 'Provide contact information for James Hardin' },
+    { name: 'Resume', description: 'A resume of James Hardin\'s web development skills' },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
