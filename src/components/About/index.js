218fd6c360fb00f51ed2efbcd7f7024da3d2e096
JamesH <jwhusc7@gmail.com>
2022-06-17 14:58:58 -0400

    mvp about me created

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import coverImage from "../../assets/images/Headshot.jpg";
+
+function About() {
+
+  return (
+    <section className="my-5 about-me" >
+      <h1 id="about">About me</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="James Hardin" />
+      <div className="my-2">
+        <p>
+          Graduate of UNCC Full Stack Web Development Bootcamp.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
