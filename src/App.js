0470745a90c7bd64ab890b87f497922b92d220a9
JamesH <jwhusc7@gmail.com>
2022-06-17 15:32:05 -0400

    added nav bar with highlighting titles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
 import './App.css';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'About Me',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'Portfolio', description: 'Portraits of people in my life' },
+    { name: 'Contact', description: 'Delicious delicacies' },
+    { name: 'Resume', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
-      <About></About>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
