6844f8da65ea7bdeedcbdce6365f88a6844959bb
JamesH <jwhusc7@gmail.com>
2022-06-21 18:57:58 -0400

    links open in new tab

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <section>
       <p>
-        <a href="https://github.com/JamesHardin02"> GitHub Profile </a>
-        <a href="https://stackoverflow.com/users/17978982/jameshardin"> Stack Overflow Profile </a>
-        <a href="https://www.linkedin.com/in/james-hardin-96487122a/"> LinkedIn Profile </a>
+        <a target="_blank" rel="noreferrer noopener" href="https://github.com/JamesHardin02"> GitHub Profile </a>
+        <a target="_blank" rel="noreferrer noopener" href="https://stackoverflow.com/users/17978982/jameshardin"> Stack Overflow Profile </a>
+        <a target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/james-hardin-96487122a/"> LinkedIn Profile </a>
       </p>      
     </section>
   )
